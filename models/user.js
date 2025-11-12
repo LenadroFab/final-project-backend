b4681bc76cadf3b0147fe3c70f3bbede3064fccc
Hendrik <hendriksiburian22@gmail.com>
2025-11-12 13:51:29 +0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,43 +1,19 @@
-// backend/models/User.js
-module.exports = (sequelize, DataTypes) => {
+// backend/models/user.js
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
   const User = sequelize.define(
     "User",
     {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          len: [3, 50],
-        },
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false, // hash sebelum simpan
-      },
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      username: { type: DataTypes.STRING, allowNull: false, unique: true },
+      password: { type: DataTypes.STRING, allowNull: false },
       role: {
         type: DataTypes.ENUM("admin", "kasir", "customer"),
-        allowNull: false,
         defaultValue: "customer",
       },
     },
-    {
-      tableName: "users",
-      timestamps: true,
-      underscored: false,
-    }
+    { tableName: "users", timestamps: true }
   );
-
-  // 🔗 asosiasi jika nanti dibutuhkan
-  User.associate = (models) => {
-    // contoh:
-    // User.hasMany(models.Order, { foreignKey: "user_id" });
-  };
-
   return User;
 };
