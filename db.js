8127714652dfcf8677134485fc4c742cf1bc790e
Hendrik <hendriksiburian22@gmail.com>
2026-01-13 20:33:54 +0700

    fix

--- db.js
+++ db.js
@@ -5,14 +5,12 @@ require("dotenv").config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   logging: false,
+  protocol: "postgres",
   dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
+    ssl: process.env.NODE_ENV === "production",
   },
   pool: {
-    max: 5,       
+    max: 3,
     min: 0,
     acquire: 30000,
     idle: 10000,
