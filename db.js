8f8d420577f12360b8def0c32df4cb743838b631
Hendrik <hendriksiburian22@gmail.com>
2026-01-13 20:40:36 +0700

    fix

--- db.js
+++ db.js
@@ -2,15 +2,19 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+if (!process.env.DATABASE_URL) {
+  console.error("❌ DATABASE_URL not found");
+  process.exit(1);
+}
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   logging: false,
-  protocol: "postgres",
   dialectOptions: {
     ssl: process.env.NODE_ENV === "production",
   },
   pool: {
-    max: 3,
+    max: 2,
     min: 0,
     acquire: 30000,
     idle: 10000,
